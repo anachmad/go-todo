7d89fafba7350a9add61b02a3e36638e65302c74
Andik Nur Achmad <andik.achmad@gmail.com>
2018-02-28 18:32:46 +0700

    add fetchSingleTodo

--- todo/main.go
+++ todo/main.go
@@ -36,7 +36,7 @@ func main() {
 	{
 		v1.POST("/", createTodo)
 		v1.GET("/", fetchAllTodo)
-		// v1.GET("/:id", fetchSingleTodo)
+		v1.GET("/:id", fetchSingleTodo)
 		// v1.PUT("/:id", updateTodo)
 		// v1.DELETE("/:id", deleteTodo)
 	}
@@ -90,4 +90,27 @@ func fetchAllTodo(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
 
+}
+
+// fetchSingleTodo fetch a single todo
+func fetchSingleTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+		return
+	}
+
+	completed := false
+	if todo.Completed == 1 {
+		completed = true
+	} else {
+		completed = false
+	}
+
+	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed}
+	c.JSON(http.StatusOK, gin.H("status": http.StatusOK, "data": _todo))
 }

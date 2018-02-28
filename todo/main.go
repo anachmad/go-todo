8e01a0a44b0292b5908eb8df457d4bea9afe105d
Andik Nur Achmad <andik.achmad@gmail.com>
2018-02-28 18:14:08 +0700

    enable routing for get all

--- todo/main.go
+++ todo/main.go
@@ -35,7 +35,7 @@ func main() {
 	v1 := router.Group("/api/v1/todos")
 	{
 		v1.POST("/", createTodo)
-		// v1.GET("/", fetchAllTodo)
+		v1.GET("/", fetchAllTodo)
 		// v1.GET("/:id", fetchSingleTodo)
 		// v1.PUT("/:id", updateTodo)
 		// v1.DELETE("/:id", deleteTodo)

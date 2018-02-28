437ebb86e1d25c67a1522bc064f24a2dc6f086ec
Andik Nur Achmad <andik.achmad@gmail.com>
2018-02-28 18:42:47 +0700

    add updateTodo

--- todo/main.go
+++ todo/main.go
@@ -114,3 +114,21 @@ func fetchSingleTodo(c *gin.Context) {
 	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todo})
 }
+
+// updateTodo update a todo
+func updateTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+		return
+	}
+
+	db.Model(&todo).Update("title", c.PostForm("title"))
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	db.Model(&todo).Update("completed", completed)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo updated successfully!"})
+}
